4f34caeeca1e39cd28a8f05739d9661ecec01496
rory <roryhibbler@hotmail.com>
2024-03-02 11:20:10 -0500

    Add animation files and driver

--- animation/anim1.c
+++ animation/anim1.c
@@ -12,9 +12,9 @@
 int main (int argc, char **argv)
 {
 
-    int height = 50;
+    int height = 100;
     int width = height;
-    int n_frames = 10;
+    int n_frames = 100;
 
     struct Canvas** frames;
 
@@ -41,6 +41,25 @@ int main (int argc, char **argv)
     fprintf(stdout,"Animating Frames!\n");
     for (i = 0; i < n_frames; i++) {
       etchCircle(frames[i],10+i,width/2,5,fuschia);
+
+
+      int j;
+      int r,g,b;
+      for (j = 0; j < width/2; j++) {
+        r = 255/2 + 255/2 * sinf((float)j/19 + (float)i/17);
+        g = 255/2 + 255/2 * sinf((float)j/17 + (float)i/19);
+        b = 255/2 + 255/2 * sinf((float)j/23 + (float)i/5);
+        struct ColorVec* darn = initColor(r,g,b);
+        etchCircle(frames[i],width/2,width/2,j,darn);
+      }
+
+
+      float grade = ((float)i / (float)n_frames) * 255;
+      struct ColorVec* dillard = initColor(100,grade,100);
+
+      drawLine(frames[i],0,0,width,width,dillard);
+
+      etchCircle(frames[i],10+i,width/2,10 + i,fuschia);
     }
 
     // Frame file-writing loop
